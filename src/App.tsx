7f8d8ba9e2c7f081cbfaef9021ae8f34a2b1a51a
Julian Rosse <julian@helixbass.net>
2022-08-04 23:14:00 -0400

    manual technique

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const logoRef = useRef<HTMLImageElement | null>(null);
+  const cacheTweenEndValuesRef = useRef<any | null>(null);
+  useEffect(() => {
+    console.log('calling effect on mount')
+    if (cacheTweenEndValuesRef.current) {
+      console.log('using cached end values for .fromTo()')
+      gsap.fromTo(logoRef.current, {
+        scale: 0.5,
+      }, cacheTweenEndValuesRef.current)
+    } else {
+      const tween = gsap.from(logoRef.current, {
+        scale: 0.5,
+      })
+      tween.progress(1)
+      const endValues: any = {}
+      console.log({endValuesPre: endValues})
+      Object.keys({scale: 0.5}).forEach(propertyName => {
+        console.log({propertyName})
+        endValues[propertyName] = gsap.getProperty(logoRef.current, propertyName)
+        console.log({endValuesDuring: endValues})
+      })
+      console.log({endValues})
+      cacheTweenEndValuesRef.current = endValues
+      tween.progress(0)
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img ref={logoRef} src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
